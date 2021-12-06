bd79197bba332833eee9c42810040b20f3bda275
Ronaldo <rds6@discente.ifpe.edu.br>
2021-12-06 11:53:39 -0300

    Exclusao da tela de login
    Adição dos botões das funcionalidades básicas e exclusão da tela de login devido a problema com funções

--- app1/funcoes.py
+++ app1/funcoes.py
@@ -1,2 +1,97 @@
-#Arquivo de funções
+from tkinter import*
+from tkinter import ttk
+from tkinter import font
+from PIL import ImageTk, Image
+from tkinter import messagebox
+
+#dicionários que serão manipulados durante a execução do programa
+dic_livros = {}
+dic_cliente = {}
+
+#botão para fechar a tela
+def sair(tela1):
+    tela1.destroy()
+
+def mostrar():
+    print(criar_tela_cadastro.id_livro.get())
+#arquivo que será usado como banco de dados e jogados nos dicionários
+arquivo_livros = open("livros.txt","a")
+arquivo_clientes = open("cliente.txt","a")
+
+
+def criar_tela_cadastro():
+    janela_cadastro = Toplevel()
+    janela_cadastro.geometry("300x300+300+100")
+
+    #Entradas do usuario qu serão salvas no dicionário
+    label_id = Label(janela_cadastro, width=10, text='ID', font="Times 15 bold italic")
+    label_id.place(x=-42,y=30)
+    id_livro = Entry(janela_cadastro, width=20)
+    id_livro.place(x=2, y=50)
+
+    label_nome = Label(janela_cadastro, width=10, height=1, text='NOME',font="Times 15 bold italic")
+    label_nome.place(x=-25,y=80)
+    nome_livro = Entry(janela_cadastro, width=20)
+    nome_livro.place(x=2, y=100)
+
+    label_ano= Label(janela_cadastro, width=10, height=1 ,text='ANO',font="Times 15 bold italic")
+    label_ano.place(x=-32,y=130)
+    ano_livro = Entry(janela_cadastro, width=20)
+    ano_livro.place(x=2, y=150)
+
+    label_autor = Label(janela_cadastro, width=10, height=1, text='AUTOR',font="Times 15 bold italic")
+    label_autor.place(x=-22,y=180)
+    autor_livro = Entry(janela_cadastro, width=20)
+    autor_livro.place(x=2, y=200)
+
+    botao_cadastrar = Button(janela_cadastro,text="CADASTRAR",command=mostrar)
+    botao_cadastrar.place(x=5, y=270)
+
+    botao_fechar = Button(janela_cadastro, text="VOLTAR", command=lambda:sair(janela_cadastro))
+    botao_fechar.place(x=235, y=270)
+    
+#def cadastrar():
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+arquivo_livros.close()
+arquivo_clientes.close()
+
+
+
 
---
+++ app1/tela_principal.py
@@ -0,0 +1,53 @@
+from tkinter import*
+from tkinter import ttk
+from PIL import ImageTk, Image
+from tkinter import messagebox
+from funcoes import*
+
+
+caminho = "PROJETO-PROGRAMACAO/app1/images/"
+
+dic_livros = {1:['Era uma vez em Hollywood','Ronaldo','2021','terror'], 2:['O Capital','Karl Marx', '1880','Sociologia']}
+
+
+janela_principal =Tk()
+janela_principal.geometry ("640x480+300+100")
+janela_principal.title("Library Sis")
+janela_principal.iconbitmap(caminho+"icon.ico")   
+janela_principal.resizable(0,0)
+
+#imagem de fundo
+img_bg = ImageTk.PhotoImage(Image.open(caminho+"bg.jpg"))
+label_tela = Label(janela_principal, image = img_bg).place(x=-5,y=0)
+
+#logo
+logo = ImageTk.PhotoImage(Image.open(caminho+"logo.png"))
+label_logo = Label(janela_principal, image = logo, borderwidth=0).place(x=220,y=2)
+
+#Imagens do botões
+imagem_pesquisa = ImageTk.PhotoImage(Image.open(caminho+"busca_2.png"))
+imagem_cadastro = ImageTk.PhotoImage(Image.open(caminho+"cadastro.png"))
+imagem_cadastro_cliente = ImageTk.PhotoImage(Image.open(caminho+"cadastro_pessoa.png"))
+imagem_venda = ImageTk.PhotoImage(Image.open(caminho+"venda.png"))
+
+#Botoes que iniciam as funcionalidades do código
+button_pesquisa = Button(janela_principal, image=imagem_pesquisa)
+button_pesquisa.place(x=80, y=130)
+
+button_cadastro = Button(janela_principal, image=imagem_cadastro,command=criar_tela_cadastro)
+button_cadastro.place(x=210, y=130)
+
+button_cadastro_cliente = Button(janela_principal, image=imagem_cadastro_cliente)
+button_cadastro_cliente.place(x=340, y=130)
+
+button_venda = Button(janela_principal,image=imagem_venda)
+button_venda.place(x=470, y=130)
+
+#Botão para sair do programa!
+botao1 = Button(janela_principal, text='Sair',font=('Helvetica', 8, 'bold'), width=10, command=lambda:sair(janela_principal))
+botao1.place(x=550, y=445)
+
+#mainloop executa o loop que permite a janela permanecer aberta
+janela_principal.mainloop()
+
+
